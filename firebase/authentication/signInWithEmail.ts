3e095d4755b1f3a299dc29c3762610632a1505e6
Sebastian Semeniuc <xsebyyy@gmail.com>
2022-05-12 11:58:30 +0300

    working on firebase authentication

--- firebase/authentication/signInWithEmail.ts
+++ firebase/authentication/signInWithEmail.ts
@@ -2,9 +2,12 @@ import { auth } from "./"
 import { signInWithEmailAndPassword } from "firebase/auth"
 
 export const signInWithEmail = async (email: string, password: string) => {
-  await signInWithEmailAndPassword(auth, email, password).then(
-    (userCredential) => {
+  await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
       const user = userCredential.user
-    }
-  )
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+    })
 }
--- firebase/authentication/signUpWithEmail.ts
+++ firebase/authentication/signUpWithEmail.ts
@@ -3,9 +3,12 @@ import { auth } from "./"
 import { createUserWithEmailAndPassword } from "firebase/auth"
 
 export const signUpWithEmail = async (email: string, password: string) => {
-  await createUserWithEmailAndPassword(auth, email, password).then(
-    (userCredential) => {
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
       const user = userCredential.user
-    }
-  )
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+    })
 }
